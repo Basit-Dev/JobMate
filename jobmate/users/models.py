b8fc4299d7a1bad29009070e7492726eebde5706
basit-dev <basitdev@icloud.com>
2025-12-01 20:27:28 +0000

    Profile model created
    Profile created and linked with Django user model

--- jobmate/users/models.py
+++ jobmate/users/models.py
@@ -1,3 +1,57 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+# Profile model: this stores extra information about users on top of django built in model
+
+class Profile(models.Model):
+
+    # User roles
+    ROLE_CHOICES = [
+        ('admin', 'Admin'),
+        ('engineer', 'Engineer'),
+    ]
+
+    # 1 to 1 relationship link to extend Djangos user model: each User gets exactly one Profile that links with Django allauth User.
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    # Profile fields
+    phone_number = models.CharField(
+        max_length=20,
+        blank=False,   
+    )
+
+    role = models.CharField(
+        max_length=20,
+        choices=ROLE_CHOICES,  
+        default='engineer'     
+    )
+
+    # Bank details
+    bank_name = models.CharField(
+        max_length=100,
+        blank=True,
+        null=True
+    )
+
+    account_holder = models.CharField(
+        max_length=100,
+        blank=True,
+        null=True
+    )
+
+    account_number = models.CharField(
+        max_length=20,
+        blank=True,
+        null=True
+    )
+
+    sort_code = models.CharField(
+        max_length=20,
+        blank=True,
+        null=True
+    )
+
+    # This displays the profile name in the admin and shell
+    def __str__(self):
+        return f"{self.user.username} Profile"
