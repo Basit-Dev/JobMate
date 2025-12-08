2897a2d1625b513feb8ff6834763e7c0fef6721e
basit-dev <basitdev@icloud.com>
2025-12-08 20:13:32 +0000

    Create job
    Registered models in admin, form.py, views.py and html updated. Form POSTS data. Message added to all jobs

---
+++ jobmate/jobs/forms/jobs.py
@@ -0,0 +1,47 @@
+from django import forms
+from jobs.models import Job
+
+# This form allows users to edit their Profile information using model form
+
+class JobForm(forms.ModelForm):
+
+    class Meta:
+        # Profile fields
+        model = Job
+        fields = [
+            'job_title',
+            'property_owner',
+            'address',
+            'city',
+            'post_code',
+            'priority',
+            'category',
+            'status',
+            'description',
+            'due_date',
+            'estimated_hours',
+            'job_cost',
+            'assigned_engineer'
+        ]
+
+        # Displays the input styles to match bootstrap
+        widgets = {
+            'job_title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter job title'}),
+            'property_owner': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Property owner name'}),
+            'address': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter full address'}),
+            'city': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'City'}),
+            'post_code': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Postcode'}),
+
+            'priority': forms.Select(attrs={'class': 'form-select'}),
+            'category': forms.Select(attrs={'class': 'form-select'}),
+            'status': forms.Select(attrs={'class': 'form-select'}),
+
+            'description': forms.Textarea(attrs={'class': 'form-control', 'rows': 4, 'placeholder': 'Job description (min 20 characters)'}),
+
+            'due_date': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
+
+            'estimated_hours': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Estimated hours'}),
+            'job_cost': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Job cost (£)'}),
+
+            'assigned_engineer': forms.Select(attrs={'class': 'form-select'}),
+        }
--- jobmate/jobs/views.py
+++ jobmate/jobs/views.py
@@ -1,20 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
+from jobs.forms.jobs import JobForm
+from django.contrib import messages
 
 # Create your views here.
+
+
 @login_required
 def all_jobs(request):
     """
     This view renders the all jobs page
     """
-    return render(request, 'all_jobs.html')  
+    return render(request, 'all_jobs.html')
 
-@login_required
-def create_job(request):
-    """
-    This view renders the create job page
-    """
-    return render(request, 'create_job.html')   
+# @login_required
+# def create_job(request):
+#     """
+#     This view renders the create job page
+#     """
+#     return render(request, 'create_job.html')
+
 
 @login_required
 def edit_job(request):
@@ -23,6 +28,7 @@ def edit_job(request):
     """
     return render(request, 'edit_job.html')
 
+
 @login_required
 def job_detail(request):
     """
@@ -30,9 +36,38 @@ def job_detail(request):
     """
     return render(request, 'job_detail.html')
 
+
 @login_required
 def delete_job(request):
     """
     This view renders the delete job page
     """
     return render(request, 'delete_job.html')
+
+# CREATE NEW JOB
+
+
+@login_required
+def create_job(request):
+
+    # POST JOB INFO
+    if request.method == "POST":
+        create_job_form = JobForm(request.POST)
+
+        if create_job_form.is_valid():
+            job = create_job_form.save(commit=False)
+            job.created_by = request.user   # attach the user
+            job.save()
+            create_job_form.save()
+            messages.success(request, 'Job was successfully created!')
+            return redirect("jobs:all_jobs")
+        else:
+            # Show error messages
+            messages.error(request, "Please correct the errors below.")
+        
+    # If NOT posting data then load the value from the instance
+    else:
+        create_job_form = JobForm()
+
+    # Load the profile page on request and pass the data to the forms on render
+    return render(request, "create_job.html", {"create_job_form": create_job_form })
