81d0537e2da6e2c935f2c33737197d227219bb19
basit-dev <basitdev@icloud.com>
2026-01-05 01:36:49 +0000

    Update context_processors.py

--- cart/context_processors.py
+++ cart/context_processors.py
@@ -3,10 +3,10 @@ from .models import Transaction
 
 # This function display in the dasboard how many items are in the basket
 def basket_item_count(request):
-    if request.user.is_authenticated and request.user.profile.role == "Admin":
+    if request.user.is_authenticated and request.user.profile.role == "admin":
         count = Transaction.objects.filter(
             status="open").count()
-    elif request.user.is_authenticated and request.user.profile.role != "Admin":
+    elif request.user.is_authenticated and request.user.profile.role != "admin":
         count = Transaction.objects.filter(
             user=request.user,
             status="open").count()
